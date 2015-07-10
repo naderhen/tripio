3ced9e05ca063f215cc9baa81a8bff012810fc9e
Nader Hendawi <naderhen@gmail.com>
2015-07-10 10:14:52 -0400

    superagent
    Signed-off-by: Nader Hendawi <naderhen@gmail.com>

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Main from './containers/Main';
 import 'semantic-ui-css/semantic.css';
 import './styles/main.scss';
+import request from 'superagent';
 
 React.render(
   <Main />,
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+
+  end
+
+  def test
+    binding.pry
   end
 end
